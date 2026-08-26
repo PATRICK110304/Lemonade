4d004aae3c9d043f80be5477adae03b1db5cd063
hydration-tracker <patrickndri120@gmail.com>
2026-08-26 16:16:58 +0000

    teste de l'application sur un emulateur

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,15 +3,32 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,22 +36,83 @@ class MainActivity : ComponentActivity() {
         setContent {
             LemonadeTheme {
                 LemonadeApp()
-                }
             }
         }
     }
-
+}
 
 @Composable
 fun LemonAppWithButtonAndImage(modifier: Modifier = Modifier) {
+    var currentStep by remember { mutableStateOf(1) }
+    var squeezeCount by remember { mutableStateOf(0) }
 
+    val imageResource = when (currentStep) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+
+    val textResource = when (currentStep) {
+        1 -> R.string.lemon_tree_to_select
+        2 -> R.string.lemon_to_squeeze
+        3 -> R.string.lemonade_to_drink
+        else -> R.string.empty_glass_to_start
+    }
+
+    val contentDescriptionResource = when (currentStep) {
+        1 -> R.string.lemon_tree_content_description
+        2 -> R.string.lemon_content_description
+        3 -> R.string.lemonade_content_description
+        else -> R.string.empty_glass_content_description
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Button(
+            onClick = {
+                when (currentStep) {
+                    1 -> {
+                        currentStep = 2
+                        squeezeCount = (2..4).random()
+                    }
+                    2 -> {
+                        squeezeCount--
+                        if (squeezeCount == 0) {
+                            currentStep = 3
+                        }
+                    }
+                    3 -> currentStep = 4
+                    4 -> currentStep = 1
+                }
+            },
+            shape = RoundedCornerShape(16.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFC3ECD2))
+        ) {
+            Image(
+                painter = painterResource(imageResource),
+                contentDescription = stringResource(contentDescriptionResource),
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = stringResource(textResource),
+            fontSize = 18.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun LemonadeApp() {
     LemonAppWithButtonAndImage(
-
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
     )
-
 }
