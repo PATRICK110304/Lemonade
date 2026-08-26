64623ba8800691eddadeddd64d79361132a1105d
hydration-tracker <patrickndri120@gmail.com>
2026-08-26 15:50:51 +0000

    creation du projet

---
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.lemonade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lemonade.ui.theme.LemonadeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LemonadeTheme {
+                LemonadeApp()
+                }
+            }
+        }
+    }
+
+
+@Composable
+fun LemonAppWithButtonAndImage(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LemonadeApp() {
+    LemonAppWithButtonAndImage(
+
+    )
+
+}
